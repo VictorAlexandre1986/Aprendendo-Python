0bb5b99f7baabb028c8f6321e24e1e115f99c463
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-08-05 17:10:49 -0300

    Add files via upload

---
+++ Exemplo1Multiprocess.py
@@ -0,0 +1,20 @@
+import multiprocessing
+
+print(f'Iniciando o processo com nome: {multiprocessing.current_process().name}')
+
+
+def faz_algo(valor):
+    print(f'Fazendo algo com o valor {valor}')
+    
+
+def main():    
+    pc = multiprocessing.Process(target=faz_algo, args=('Passaro',), name='Processo Victor')
+    
+    print(f'Iniciando o processo com o nome..: {pc.name}')
+
+    pc.start()
+    pc.join()
+
+if __name__=='__main__':
+    main()
+
