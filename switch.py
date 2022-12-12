f78b264441f9051771b2dbdcf54e7173e22ba3fc
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2022-12-12 19:15:42 -0300

    Add files via upload

---
+++ switch.py
@@ -0,0 +1,19 @@
+dia = 1
+
+match dia:
+    case 1:
+        print('Domingo')
+    case 2:
+        print('Segunda')
+    case 3:
+        print('Terça')
+    case 4:
+        print('Quarta')
+    case 5:
+        print('Quinta')
+    case 6:
+        print('Sexta')
+    case 7:
+        print('Sábado')
+    case _:
+        print('Não existe')
